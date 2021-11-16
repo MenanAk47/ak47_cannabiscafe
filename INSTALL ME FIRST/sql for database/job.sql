4817d2e7d477d496fb81239775622effe1e7833f
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-17 04:13:17 +0600

    Update job.sql

--- INSTALL ME FIRST/sql for database/job.sql
+++ INSTALL ME FIRST/sql for database/job.sql
@@ -1,11 +1,14 @@
+DELETE FROM `addon_account` WHERE `name` = 'society_cannabiscafe';
 INSERT INTO `addon_account` (name, label, shared) VALUES
 	('society_cannabiscafe', 'Cannabis Cafe', 1)
 ;
 
+DELETE FROM `jobs` WHERE `name` = 'cannabiscafe';
 INSERT INTO `jobs` (name, label) VALUES
 	('cannabiscafe', 'Cannabis Cafe')
 ;
 
+DELETE FROM `job_grades` WHERE `job_name` = 'cannabiscafe';
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
 	('cannabiscafe',0,'farmer','Farmer',20,'{}','{}'),
 	('cannabiscafe',1,'shopkeeper','Shopkeeper',40,'{}','{}'),
